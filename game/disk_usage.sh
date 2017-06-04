22932c39d1ab2c9d926ad5513df0ef444b6fe0fb
alpine9000 <alex.g.brown@gmail.com>
2017-06-04 11:25:34 +1000

    added fast ram usage stats

--- game/disk_usage.sh
+++ game/disk_usage.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 endFast=$((16#`cat debugger.syms | grep _endFast | sed 's/0x//' | sed 's/_endFast: //'`))
 startFast=$((16#`cat debugger.syms | grep _startFast | sed 's/0x//' | sed 's/_startFast: //'`))
-echo "CHIP RAM USAGE:" $((16#`cat debugger.syms | grep endRam | sed 's/0x//' | sed 's/endRam: //'`))
-echo "FAST RAM USAGE:" $(($endFast-$startFast))
+fastUsed=$(($endFast-$startFast))
+chipUsed=$((16#`cat debugger.syms | grep endRam | sed 's/0x//' | sed 's/endRam: //'`))
 cat out/fast/bootblock.txt | grep ", value " | cut -d " " -f3,7 | cut -d "," -f1 > /tmp/bootblock.symbols
-echo "    DISK USAGE:" $((16#`cat /tmp/bootblock.symbols | grep maxDisk | sed 's/0x//' | sed 's/maxDisk: //'`))
+diskUsed=$((16#`cat /tmp/bootblock.symbols | grep maxDisk | sed 's/0x//' | sed 's/maxDisk: //'`))
+
+echo "CHIP RAM USAGE:" $chipUsed" ("$((524288-$chipUsed))" free)"
+echo "FAST RAM USAGE:" $fastUsed" ("$((524288-$fastUsed))" free)"
+echo "    DISK USAGE:" $diskUsed" ("$((895488-$diskUsed))" free)" #901120 - hiscore track
 
 
 
