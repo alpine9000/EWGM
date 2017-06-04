1bd0cbe5e6e0e38771b04660cacd210d77ba9560
alpine9000 <alex.g.brown@gmail.com>
2017-06-04 11:12:21 +1000

    added fast ram usage stats

--- game/disk_usage.sh
+++ game/disk_usage.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+endFast=$((16#`cat debugger.syms | grep _endFast | sed 's/0x//' | sed 's/_endFast: //'`))
+startFast=$((16#`cat debugger.syms | grep _startFast | sed 's/0x//' | sed 's/_startFast: //'`))
 echo "CHIP RAM USAGE:" $((16#`cat debugger.syms | grep endRam | sed 's/0x//' | sed 's/endRam: //'`))
+echo "FAST RAM USAGE:" $(($endFast-$startFast))
 cat out/fast/bootblock.txt | grep ", value " | cut -d " " -f3,7 | cut -d "," -f1 > /tmp/bootblock.symbols
-echo "DISK USAGE:" $((16#`cat /tmp/bootblock.symbols | grep maxDisk | sed 's/0x//' | sed 's/maxDisk: //'`))
+echo "    DISK USAGE:" $((16#`cat /tmp/bootblock.symbols | grep maxDisk | sed 's/0x//' | sed 's/maxDisk: //'`))
+
+
+
 
